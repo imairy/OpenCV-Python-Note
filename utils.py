1f8865b788a42e7f1882fdfeec8821cc6591501e
imairy <imairy@163.com>
2020-06-14 18:41:24 +0800

    day11-12

--- utils.py
+++ utils.py
@@ -3,10 +3,7 @@ from matplotlib import pylab as plt
 def imshow(mat):
     mv = cv.split(mat)
     if len(mv) == 3:
-        b, g, r = mv
-        mv = [r, g, b]
-        dst = cv.merge(mv)
-    else:
-        dst = cv.merge(mv)
+        mv = mv[::-1]
+    dst = cv.merge(mv)
     plt.imshow(dst)
     # return dst
